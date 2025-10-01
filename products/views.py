6f5c4eeb52e88ec8e1f6007cfcf3096493b442da
Rex-David Buchi <107920648+RexDavid06@users.noreply.github.com>
2025-10-01 23:33:39 +0100

    added the Category and Product Model

--- products/views.py
+++ products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .serializers import ProductSerializer, CategorySerializer
+from .models import Product, Category
+from rest_framework import generics
 
 # Create your views here.
+
