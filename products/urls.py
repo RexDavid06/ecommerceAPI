01c4ecf803852e484417ae770dd1a5a7930af439
Rex-David Buchi <107920648+RexDavid06@users.noreply.github.com>
2025-10-02 03:29:22 +0100

    added the product and category ednpoints

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('categories/', views.CategoryListView.as_view(), name='category-list'),
+    path('category/create/', views.CategoryCreateView.as_view(), name='category-create'),
+    path('category/update/<int:id>/', views.CategoryRetrieveUpdateDestroyView.as_view(), name='category-update'),
+    path('', views.ProductListView.as_view(), name='products'),
+    path('create/', views.ProductCreateView.as_view(), name='product-create'),
+    path('/update/<int:id>/', views.ProductRetrieveUpdateDestroyView.as_view(), name='product-update')
 ]
--- products/views.py
+++ products/views.py
@@ -2,6 +2,41 @@ from django.shortcuts import render
 from .serializers import ProductSerializer, CategorySerializer
 from .models import Product, Category
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 
 # Create your views here.
+
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny]
+
+class CategoryCreateView(generics.CreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminUser]
+
+class CategoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'id'
+    permission_classes = [IsAdminUser]
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [AllowAny]
+
+class ProductCreateView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminUser]
+
+class ProductRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'id'
+    permission_classes = [IsAdminUser]
+
+
 
