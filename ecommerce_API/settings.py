780b72684247f64e465f5f372fa2f59de75185bf
Rex-David Buchi <107920648+RexDavid06@users.noreply.github.com>
2025-10-03 11:24:22 +0100

    feat: added search, pagination, and ordering

--- ecommerce_API/settings.py
+++ ecommerce_API/settings.py
@@ -46,13 +46,17 @@ INSTALLED_APPS = [
     'payments',
     'products',
     'users.apps.UsersConfig',
+    'django_filters',
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- products/views.py
+++ products/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .serializers import ProductSerializer, CategorySerializer
 from .models import Product, Category
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 
@@ -23,9 +24,17 @@ class CategoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAdminUser]
 
 class ProductListView(generics.ListAPIView):
-    queryset = Product.objects.all()
+    queryset = Product.objects.filter(is_available=True)
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
     serializer_class = ProductSerializer
     permission_classes = [AllowAny]
+    #enable search by name and description
+    search_fields = ['name', 'description']
+    #enable filtering by category and price
+    filterset_fields = ['category', 'price']
+    #enable ordering by price and created_at
+    ordering_fields = ['price', 'created_at']
+    ordering = ['-created_at']
 
 class ProductCreateView(generics.CreateAPIView):
     queryset = Product.objects.all()
